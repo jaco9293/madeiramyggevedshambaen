7435b0c841df57c0cf7b2438a16024d529f710b0
Jacob Bloch Johansen <JacobBloch@karl-johan.kea.local>
2017-03-17 09:09:21 +0100

    en masse lækkert

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var map;
 
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -9,10 +10,77 @@ function initMap() {
             lng: 12.598767
         },
         zoom: 17
-
     });
+
+
+    // watch Position
+
+    /* if (navigator.geolocation) {
+         navigator.geolocation.watchPosition(function (position) {
+             var minPos = {
+                 lat: position.coords.latitude,
+                 lng: position.coords.longitude
+             };
+             map.setCenter(minPos);
+             var mig = new google.maps.Marker({
+                 position: minPos,
+                 map: map,
+             });
+         });
+     } */
+
+    // Get JSON
+
+    $.getJSON("kbhture.json", visListe);
+
+
+    //Opdel liste
+    function visListe(infoOmSteder) {
+        console.table(infoOmSteder);
+        infoOmSteder.forEach(visSteder);
+    }
+    //Lav klon
+    function visSteder(sted) {
+
+        var marker = new google.maps.Marker({
+            position: sted.position,
+            map: map,
+            title: sted.navn,
+        });
+
+
+        var infowindow = new google.maps.InfoWindow({});
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+            infowindow.setContent(klon);
+
+
+        });
+        var klon = document.querySelector("#my_template").content.cloneNode(true);
+
+        klon.querySelector(".data_overskrift").innerHTML = sted.navn;
+        klon.querySelector(".data_beskrivelse").innerHTML = sted.beskrivelse;
+        klon.querySelector(".data_billede").src = "img/" + sted.billede + ".jpg";
+
+
+        console.log(sted.position);
+
+    }
+
+    // infowindow
+
+    // var klon = document.querySelector("#my_template").content.cloneNode(true);
+
+    //klon.querySelector(".data_overskrift").innerHTML = sted.navn;
+    //klon.querySelector(".data_beskrivelse").innerHTML = sted.beskrivelse;
+    //klon.querySelector(".data_billede").src = "img/" + sted.billede + ".jpg";
+
+
+    //console.log(sted.position);
+
 }
 
+// Menu
 $(".arrow").on("click", drop);
 
 function drop() {
---
+++ quiz.js
@@ -0,0 +1,39 @@
+var jsonData;
+
+// Hent JSON
+$.getJSON("quiz.json", visQuizListe);
+
+//Opdel liste
+function visQuizListe(infoOmQuiz) {
+    console.table(infoOmQuiz);
+
+    jsonData = infoOmQuiz;
+
+    //infoOmQuiz.forEach(visQuiz);
+    visQuiz(infoOmQuiz[1]);
+}
+//Lav klon
+function visQuiz(data) {
+
+    //Indsæt data i klon
+    var klon = document.querySelector("#quiz_template").content.cloneNode(true);
+
+    klon.querySelector(".data_svar1").innerHTML = data.svar1;
+    klon.querySelector(".data_svar2").innerHTML = data.svar2;
+    klon.querySelector(".data_svar3").innerHTML = data.svar3;
+    klon.querySelector(".data_spoergsmaal").innerHTML = data.spoergsmaal;
+
+    document.querySelector(".quiz_spm").appendChild(klon);
+
+}
+
+
+// Menu
+$(".arrow").on("click", drop);
+
+function drop() {
+    console.log("menu drop down");
+    $(".arrowright").toggleClass("dropdownright");
+    $(".arrowleft").toggleClass("dropdownleft");
+    $(".dropmenu").slideToggle();
+}
